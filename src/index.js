0b64dcc997b7712ef7eae040b9d7cc11eb6ebac0
camiloramirezf@hotmail.com <camiloramirezf@hotmail.com>
2018-08-08 21:12:16 +0200

    add react router dom dependency

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Root from './screens/Root';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
